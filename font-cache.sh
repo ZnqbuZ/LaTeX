ab2d6c97a8038f178ec9355271362b03f9684bca
Luna Yao <40349250+ZnqbuZ@users.noreply.github.com>
2025-03-23 11:23:22 +0000

    Fix ln command

--- font-cache.sh
+++ font-cache.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd "$(dirname "$(realpath "$0")")" || exit 1
-ln -sf /etc/fonts/conf.d/09-LaTeX.conf $(pwd)/09-LaTeX.conf
-ln -sf /etc/fonts/conf.d/09-texlive.conf $(ls -d /usr/local/texlive/*/texmf-var/fonts/conf/texlive-fontconfig.conf 2>/dev/null | tail -n 1)
+ln -sf $(pwd)/09-LaTeX.conf /etc/fonts/conf.d/09-LaTeX.conf
+ln -sf $(ls -d /usr/local/texlive/*/texmf-var/fonts/conf/texlive-fontconfig.conf 2>/dev/null | tail -n 1) /etc/fonts/conf.d/09-texlive.conf 
 fc-cache -f -s -v
