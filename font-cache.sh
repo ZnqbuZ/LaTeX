94794ab2e9b329856d97a6fc7096c632768acac5
Luna Yao <40349250+ZnqbuZ@users.noreply.github.com>
2025-03-23 11:36:29 +0000

    Runtime gen & write conf

--- font-cache.sh
+++ font-cache.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
 cd "$(dirname "$(realpath "$0")")" || exit 1
-ln -sf $(pwd)/09-LaTeX.conf /etc/fonts/conf.d/09-LaTeX.conf
-ln -sf $(ls -d /usr/local/texlive/*/texmf-var/fonts/conf/texlive-fontconfig.conf 2>/dev/null | tail -n 1) /etc/fonts/conf.d/09-texlive.conf 
+
+cat <<EOF > /etc/fonts/conf.d/09-LaTeX.conf
+<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+	<dir>$(pwd)/fonts</dir>
+</fontconfig>
+EOF
+
+ln -sf $(ls -d /usr/local/texlive/*/texmf-var/fonts/conf/texlive-fontconfig.conf 2>/dev/null | tail -n 1) /etc/fonts/conf.d/09-texlive.conf
+
 fc-cache -f -s -v
