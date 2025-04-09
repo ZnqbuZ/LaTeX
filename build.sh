646cb20ec032e533660d0fd0a2d41d1bc35f67f2
Luna Yao <40349250+ZnqbuZ@users.noreply.github.com>
2025-04-09 17:59:49 +0000

    Remove ctex from build script

--- build.sh
+++ build.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 WORK_DIR="precompile"
-CLASSES="article book ctexart ctexbook"
+CLASSES="article book"
 
 cd "$(dirname "$(realpath "$0")")/${WORK_DIR}" || exit 1
 echo "Working directory: $(pwd)"
 for class in ${CLASSES}; do
     echo "Generating ${class}.sty ..."
+    CLASS_OPT="11pt"
     cat <<EOF > "${class}.sty"
 % xelatex -ini -jobname=${class} "&xelatex ${class}.sty\dump"
 
 \RequirePackage[OT1]{fontenc}
 
-\documentclass[11pt]{article}
+\documentclass[${CLASS_OPT}]{${class}}
 
 \RequirePackage{../common}
 EOF
